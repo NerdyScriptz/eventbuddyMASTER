2e02c6d0e8307f1dd16acae4b9059555eb447162
Gamer 1 <65615643+gamer167@users.noreply.github.com>
2024-05-26 19:49:18 -0400

    Update scripts.js

--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -22,3 +22,23 @@ $(document).ready(function() {
 
     loadVideos();
 });
+
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+window.alert("No stealing our code!");
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+window.alert("No stealing our code!");
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+window.alert("No stealing our code!");
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+window.alert("No stealing our code!");
+return false;
+}
+}
+//Used to block inspect element with keys
