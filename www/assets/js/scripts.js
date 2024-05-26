d2d58eaac36c74a4f80baf77eba3a7401ff94872
Jose Moran Urena <jose@josemoranurena.tech>
2024-05-26 17:44:29 -0400

    Update scripts.js

--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -1,24 +1,16 @@
 $(document).ready(function() {
-    function sanitizeInput(input) {
-        return DOMPurify.sanitize(input);
-    }
-
     function loadVideos() {
         $.getJSON('video_data.json', function(data) {
             let videoContainer = $('#video-container');
             videoContainer.empty();
             data.videos.forEach(function(video) {
-                let sanitizedThumbnail = sanitizeInput(video.thumbnail);
-                let sanitizedTitle = sanitizeInput(video.title);
-                let sanitizedDescription = sanitizeInput(video.description);
-
                 let videoCard = `
                     <div class="col-md-4">
                         <div class="video-card">
-                            <img src="${sanitizedThumbnail}" class="video-thumbnail" alt="${sanitizedTitle}">
+                            <img src="${video.thumbnail}" class="video-thumbnail" alt="${video.title}">
                             <div class="video-info">
-                                <h3>${sanitizedTitle}</h3>
-                                <p>${sanitizedDescription}</p>
+                                <h3>${video.title}</h3>
+                                <p>${video.description}</p>
                             </div>
                         </div>
                     </div>
