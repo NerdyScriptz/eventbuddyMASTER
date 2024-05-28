48de3045c0e0cc7b511dbd0381f022d23a41d294
Gamer 1 <65615643+gamer167@users.noreply.github.com>
2024-05-28 10:30:36 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { join } = require("path");
+const app = express();
+
+// Serve static assets from the /www folder
+app.use(express.static(join(__dirname, "www")));
+
+// Endpoint to serve the configuration file
+app.get("/auth_config.json", (req, res) => {
+  res.sendFile(join(__dirname, "www", "auth-config.json"));
+});
+
+// Serve the index page for all other requests
+app.get("/*", (_, res) => {
+  res.sendFile(join(__dirname, "www", "index.php"));
+});
+
+// Listen on port 3000
+app.listen(3000, () => console.log("Application running on port 3000"));
