8a686def37d6d42bde4fcb4f03781dd0607d82e6
Gamer 1 <65615643+gamer167@users.noreply.github.com>
2024-05-28 10:43:37 -0400

    Update app.js

--- www/assets/js/login/app.js
+++ www/assets/js/login/app.js
@@ -1,1 +1,71 @@
 <script src="https://cdn.auth0.com/js/auth0-spa-js/2.0/auth0-spa-js.production.js"></script>
+import { createAuth0Client } from '@auth0/auth0-spa-js';
+
+let auth0Client = null;
+
+const fetchAuthConfig = () => fetch("/www/auth_config.json");
+
+// ..
+
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0Client = await auth0.createAuth0Client({
+    domain: config.domain,
+    clientId: config.clientId
+  });
+};
+
+
+// ..
+
+window.onload = async () => {
+
+  await configureClient();
+
+  updateUI();
+
+  const isAuthenticated = await auth0Client.isAuthenticated();
+
+  if (isAuthenticated) {
+    // show the gated content
+    return;
+  }
+
+  // NEW - check for the code and state parameters
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+
+    // Process the login state
+    await auth0Client.handleRedirectCallback();
+    
+    updateUI();
+
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, "/");
+  }
+};
+
+// ..
+
+// NEW
+const updateUI = async () => {
+  const isAuthenticated = await auth0Client.isAuthenticated();
+
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-login").disabled = isAuthenticated;
+};
+
+// ..
+
+const login = async () => {
+  await auth0Client.loginWithRedirect({
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  });
+};
+
+// ..
+
